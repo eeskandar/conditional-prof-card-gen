3e0fddbbd095c20a9df4b8700af67131f6b12fb0
Iskandar <seik747@gmail.com>
2022-09-20 15:08:24 +0000

    fixing broken default links to properly display the info

--- src/js/app.js
+++ src/js/app.js
@@ -40,7 +40,9 @@ function render(variables = {}) {
   if (variables.city == null) variables.city = "Narnia";
   if (variables.country == null) variables.country = "The Wardrobe";
   if (variables.twitter == null) variables.twitter = "4geeksacademy";
-  if (variables.linkedin == null) variables.linkedin = "4geeksacademy";
+  if (variables.linkedin == null) variables.linkedin = "school/4geeksacademy";
+  if (variables.linkedin != null)
+    variables.linkedin = `in/${variables.linkedin}`;
   if (variables.instagram == null) variables.instagram = "4geeksacademy";
 
   // reset the website body with the new html output
@@ -52,7 +54,7 @@ function render(variables = {}) {
           <h3>${variables.city}, ${variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href=${variables.github}><i class="fab fa-github"></i></a></li>
+            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
             <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
           </ul>
