1415a8afdce2caf92587d7243fb74770ed3d285f
Iskandar <seik747@gmail.com>
2022-09-20 21:21:57 +0000

    cleaning the code

--- src/js/app.js
+++ src/js/app.js
@@ -1,63 +1,42 @@
 import "../style/index.css";
 
-/**
- *  EDIT ONLY INSIDE THIS RENDER FUNCTION
- *  This function is called every time the user changes types or changes any input
- * 
-    {
-        includeCover: true, // if includeCover is true the algorithm should
-        background: "https://images.unsplash.com/photo-1511974035430-5de47d3b95da", // this is the url of the image that will used as background for the profile cover
-        avatarURL: "https://randomuser.me/api/portraits/women/42.jpg", // this is the url for the profile avatar
-        socialMediaPosition: "left", // social media bar position (left or right)
-        
-        twitter: null, // social media usernames
-        github: null,
-        linkedin: null,
-        instagram: null,
-
-        name: null,
-        lastname: null,
-        role: null,
-        country: null,
-        city: null
-    }
- */
 function render(variables = {}) {
-  console.log("These are the current variables: ", variables); //print on the console
-  // here we ask the logical questions to make decisions on how to build the html
-  // if includeCover==false then we reset the cover code without the <img> tag to make the cover transparent.
+  console.log("These are the current variables: ", variables);
   let cover = `<div class="cover"><img src="${variables.background}" /></div>`;
   if (variables.includeCover == false) cover = "<div class='cover'></div>";
-  if (variables.name == null) variables.name = "John";
   if (variables.name !== null && variables.name.trim() == "") {
     variables.name = "Muchacho";
   }
-  if (variables.lastname == null) variables.lastname = "Doe";
   if (variables.lastname !== null && variables.lastname.trim() == "") {
     variables.lastname = "Chistosito";
   }
-  if (variables.role == null) variables.role = "Grand Mage";
-  if (variables.city == null) variables.city = "Narnia";
-  if (variables.country == null) variables.country = "The Wardrobe";
-  if (variables.twitter == null) variables.twitter = "4geeksacademy";
-  if (variables.linkedin != null) {
-    variables.linkedin = `in/${variables.linkedin}`;
-  }
-  if (variables.linkedin == null) variables.linkedin = "school/4geeksacademy";
-  if (variables.instagram == null) variables.instagram = "4geeksacademy";
 
-  // reset the website body with the new html output
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastname}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city}, ${variables.country}</h3>
+          <h1>${variables.name == null ? "John" : variables.name}
+          ${variables.lastname == null ? "Doe" : variables.lastname}
+          </h1>
+          <h2>${variables.role == null ? "Grand Mage" : variables.role}</h2>
+          <h3>${variables.city == null ? "Narnia" : variables.city}, 
+          ${variables.country == null ? "The Wardrobe" : variables.country}</h3>
           <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+            <li><a href="https://twitter.com/${
+              variables.twitter == null ? "4geeksacademy" : variables.twitter
+            }"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${
+              variables.github
+            }"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/${
+              variables.linkedin == null
+                ? "school/4geeksacademy"
+                : `in/${variables.linkedin}`
+            }"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${
+              variables.instagram == null
+                ? "4geeksacademy"
+                : variables.instagram
+            }"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
